147336779aaecb2b72a84bb69a21a17d27d4b22d
wu_double <wu_double@qq.com>
2026-02-11 07:09:33 +0800

    更新下readme

--- qms-frontend/src/main.ts
+++ qms-frontend/src/main.ts
@@ -1,7 +1,7 @@
 // src/main.ts
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router' // 1. 引入路由
+import router from './router' 
 
 const app = createApp(App)
 
